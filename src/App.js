68eda5471935d6c69f6320b45eee8647196b7454
andyrod <andyrod209@gmail.com>
2022-02-24 12:35:57 -0800

    Tested LoginForm and changed email to username

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@
 import './App.css';
 import RegistrationForm from './RegistrationForm/RegistrationForm';
 import React, { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LoginForm from './LoginForm/LoginForm';
 // import axios from 'axios';
 
 function App() {
 
   // const [user, setUser] = useState(null);
 
-  //   useEffect(() => {
-  //       const jwt = localStorage.getItem('token');
-  //       try {
-  //           const decodedUser = jwt_decode(jwt);
-  //           setUser(decodedUser);
-  //       } catch {}
-  //   }, [])
+    // gets Token for users
+    // useEffect(() => {
+    //     const jwt = localStorage.getItem('token');
+    //     try {
+    //         const decodedUser = jwt_decode(jwt);
+    //         setUser(decodedUser);
+    //     } catch {}
+    // }, []);
 
 
 
@@ -22,7 +25,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>hello world</h1>
-        <RegistrationForm />
+        <Routes>
+          {/* <Route path='/profile' element={() => {
+            if (!user) {
+              return <LoginForm />
+            }
+            else {
+              return <RegistrationForm user={user} />
+            }
+          }} /> */}
+          <Route path='/register' element={<RegistrationForm />} />
+          <Route path='/login' element={<LoginForm />} />
+        </Routes>
       </header>
     </div>
   );
--- src/LoginForm/LoginForm.jsx
+++ src/LoginForm/LoginForm.jsx
@@ -19,12 +19,12 @@ const LoginForm = (props) => {
     return ( 
 
         <Form onSubmit={handleSubmit } className="rounded p-4 p-sm-3">
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" placeholder="Enter Email" onChange={(event) => setUserName(event.target.value)}/>
+            <Form.Group className="mb-3">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="username" placeholder="Enter Email" onChange={(event) => setUserName(event.target.value)}/>
                 <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-3">
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="password" onChange={(event) => setPassword(event.target.value)}/>
             </Form.Group>
@@ -33,4 +33,4 @@ const LoginForm = (props) => {
     );
 }
  
-export default LoginForm;props
+export default LoginForm;
