4c5dc42819acc026f380106a3d34967043041519
Bryce Payne <bryce.payne.97@gmail.com>
2022-02-24 14:08:10 -0600

    finished the login form component

--- src/LoginForm/LoginForm.jsx
+++ src/LoginForm/LoginForm.jsx
@@ -1,30 +1,35 @@
 import { useEffect, useState } from "react";
-import jwt_decode from 'jwt-decode';
+import { Form, Button } from 'react-bootstrap'
 
 const LoginForm = (props) => {
 
     
-    const [userName, setUserName] = useState('');
+    const [username, setUserName] = useState('');
     const [password, setPassword] = useState('');
 
     function handleSubmit(event) {
         event.preventDefault();
+        let currentUser = {
+            username: username,
+            password: password
+        }
 
     }
 
     return ( 
 
-        <form className="form-grid">
-            <h3>Create User</h3>
-                <div className="form-control">
-                    <label>Username</label>
-                    <input type='text' className='form-control'/>
-                    <label>Password</label>
-                    <input type='text' className='form-control'/>
-                </div>
-            <button variant='primary'>Create User</button>
-        </form>
-
+        <Form onSubmit={handleSubmit } className="rounded p-4 p-sm-3">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email" placeholder="Enter Email" onChange={(event) => setUserName(event.target.value)}/>
+                <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="password" onChange={(event) => setPassword(event.target.value)}/>
+            </Form.Group>
+            <Button variant="primary" type="submit">Login</Button>
+        </Form>
     );
 }
  
