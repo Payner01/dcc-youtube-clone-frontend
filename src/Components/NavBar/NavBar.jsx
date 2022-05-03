6f2fb350cdeefee86bd85c72522e0d94b8104b3b
andyrod <andyrod209@gmail.com>
2022-05-02 20:20:58 -0700

    Testing trying to pull

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Navbar, Container, Nav, Form, Button, FormControl, } from 'react-bootstrap'
+import { Navbar, Container, Nav, Form, Button, FormControl, } from 'react-router-dom'
 import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom"
 
